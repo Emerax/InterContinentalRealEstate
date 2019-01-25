b60b90509f46518ff236e7d51ee2dbe18e2d9dd3
TheZoq2 <frans.skarman@gmail.com>
2019-01-25 22:34:19 +0100

    Add missile physics

---
+++ InterContinentalRealEstate/Assets/Missile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    Vector3 velocity = Vector3.one * 0.1F;
+    float g = 0.1F;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 acceleration = gameObject.transform.position.normalized * (-g) * Time.deltaTime;
+
+        velocity += acceleration;
+
+        gameObject.transform.position += velocity * Time.deltaTime;
+        Debug.Log(gameObject.transform.position);
+    }
+}
