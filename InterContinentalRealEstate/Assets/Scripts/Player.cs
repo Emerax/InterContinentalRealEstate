da7200b435c353bf1ea8889469599cebab413b85
Erik Mansén <erima668@student.liu.se>
2019-01-26 16:51:09 +0100

    Enable Player to fire a missile from a silo

--- InterContinentalRealEstate/Assets/Scripts/Player.cs
+++ InterContinentalRealEstate/Assets/Scripts/Player.cs
@@ -8,45 +8,61 @@ public class Player : MonoBehaviour {
     public float movementSpeed;
     public float minDist, maxDist;
     public GameObject planet;
+    public GameObject silo;
+
+    public GameObject missilePrefab;
+    private bool missileLaunched = false;
 
     // Start is called before the first frame update
     void Start() {
-        if (!planet) {
-            Debug.LogError("Planet not set in player!");
-        }
     }
 
     // Update is called once per frame
     void Update() {
         UpdateOrbitalMovement();
+        ReadInput();
     }
 
     void UpdateOrbitalMovement() {
-        float horizontalTranslation = Input.GetAxis("Mouse X");
-        float verticalTranslation = Input.GetAxis("Mouse Y");
+        if (!missileLaunched) {
+            float horizontalTranslation = Input.GetAxis("Mouse X");
+            float verticalTranslation = Input.GetAxis("Mouse Y");
 
-        float movement = Input.GetAxisRaw("Mouse ScrollWheel");
+            float movement = Input.GetAxisRaw("Mouse ScrollWheel");
 
-        Debug.Log(movement);
 
-        Vector3 direction = (planet.transform.position - transform.position).normalized;
-        float distance = Vector3.Distance(transform.position, planet.transform.position);
+            Vector3 direction = (planet.transform.position - transform.position).normalized;
+            float distance = Vector3.Distance(transform.position, planet.transform.position);
 
-        if(movement > 0 && distance > minDist) {
-            Debug.Log("Moving in");
-            transform.Translate(direction * movementSpeed * movement);
-        } else if (movement < 0 && distance < maxDist) {
-            Debug.Log("Moving out");
-            transform.Translate(direction * movementSpeed * movement);
+            if (movement > 0 && distance > minDist) {
+                transform.Translate(direction * movementSpeed * movement);
+            } else if (movement < 0 && distance < maxDist) {
+                transform.Translate(direction * movementSpeed * movement);
+            }
+
+
+            if (distance > minDist && distance < maxDist) {
+            }
+
+            transform.RotateAround(planet.transform.position, Vector3.up, horizontalTranslation * Time.deltaTime * horizontalCameraSpeed);
+            transform.RotateAround(planet.transform.position, Vector3.right, -verticalTranslation * Time.deltaTime * verticalCameraSpeed);
+
+            transform.LookAt(planet.transform.position);
         }
+    }
 
-
-        if (distance > minDist && distance < maxDist) {
+    void ReadInput() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (!missileLaunched) {
+                //FIRE ZHE MIZZILEZ
+                GameObject missile = Instantiate(missilePrefab, silo.transform.position + Vector3.up, silo.transform.rotation);
+                missile.GetComponent<Missile>().SetOwner(this);
+                missileLaunched = true;
+            }
         }
+    }
 
-        transform.RotateAround(planet.transform.position, Vector3.up, horizontalTranslation * Time.deltaTime * horizontalCameraSpeed);
-        transform.RotateAround(planet.transform.position, Vector3.right, -verticalTranslation * Time.deltaTime * verticalCameraSpeed);
-
-        transform.LookAt(planet.transform.position);
+    public void ReportMissileHit() {
+        missileLaunched = false;
     }
 }
