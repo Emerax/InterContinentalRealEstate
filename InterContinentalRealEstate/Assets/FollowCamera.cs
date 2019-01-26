ea5230a24a0e893f305598d8df099c6881c901a9
TheZoq2 <frans.skarman@gmail.com>
2019-01-26 11:27:16 +0100

    Improve projectile

---
+++ InterContinentalRealEstate/Assets/FollowCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    Vector3 initialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Find the missile object
+        var missile = GameObject.Find("Missile");
+        if(missile != null) {
+            var missileComponent = missile.GetComponent(typeof(Missile)) as Missile;
+            var distance = missileComponent.CameraDistanceRatio();
+        }
+    }
+}
--- InterContinentalRealEstate/Assets/Missile.cs
+++ InterContinentalRealEstate/Assets/Missile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Missile : MonoBehaviour
 {
-    Vector3 velocity = new Vector3(0, 0, 1) * 0.4F;
+    Vector3 velocity = new Vector3(1, 0, -1) * 0.4F;
     float g = 0.2F;
 
     float steering_amonut = 1F;
@@ -34,20 +34,42 @@ public class Missile : MonoBehaviour
 
         transform.rotation = Quaternion.LookRotation(velocity, transform.position);
 
-        float xMove = Input.GetAxis("Mouse X");
-        float yMove = Input.GetAxis("Mouse Y");
+        if(IsFalling()) {
+            float xMove = Input.GetAxis("Mouse X");
+            float yMove = Input.GetAxis("Mouse Y");
 
-        // Build a new local vector to use for rotateTo
-        var velocityLocal = new Vector3(xMove, yMove, 1);
+            // Build a new local vector to use for rotateTo
+            var velocityLocal = new Vector3(xMove, yMove, 1);
 
-        // Transform that vector into the global space
-        var targetVelocityWorld = transform.localToWorldMatrix * velocityLocal;
+            // Transform that vector into the global space
+            var targetVelocityWorld = transform.localToWorldMatrix * velocityLocal;
 
-        velocity = Vector3.RotateTowards(
-            velocity,
-            targetVelocityWorld,
-            steering_amonut * Time.deltaTime,
-            0.0F
-        );
+            velocity = Vector3.RotateTowards(
+                velocity,
+                targetVelocityWorld,
+                steering_amonut * Time.deltaTime,
+                0.0F
+            );
+        }
+
+        Debug.Log(CameraDistanceRatio());
+    }
+
+    bool IsFalling() {
+        return Vector3.Angle(transform.position, velocity) > 90;
+    }
+
+    float RatioToTop() {
+        var projected = Vector3.Project(velocity, transform.position);
+        return System.Math.Min(projected.magnitude * 5, 1.0F);
+    }
+
+    public float CameraDistanceRatio() {
+        if(IsFalling()) {
+            return 0;
+        }
+        else {
+            return RatioToTop();
+        }
     }
 }
