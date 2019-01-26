8fbfef5b44aa204ec4bc528ee6fe8fab66d878c7
TheZoq2 <frans.skarman@gmail.com>
2019-01-26 16:05:22 +0100

    Turn houses into bouncy houses

--- InterContinentalRealEstate/Assets/House.cs
+++ InterContinentalRealEstate/Assets/House.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class House : MonoBehaviour
 {
-    float scaleAccelerationFactor = 10F;
     float scaleVelocity = 1;
     float scale = 0;
+    float extraScale = 1.5F;
+    float time = 0;
+    const float duration = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +20,19 @@ public class House : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scale = Mathf.SmoothDamp(scale, 50, ref scaleVelocity, 100);
+        time += Time.deltaTime;
+
+        scale = EaseOutElastic(time * 1.5F);
         transform.localScale = Vector3.one * scale;
+    }
+
+    float EaseOutElastic(float t) {
+        if(t == 0) {
+            t = 0.001F;
+        }
+        var sinVal = (float) Math.Sin(Math.PI * 8 * t);
+        var timeClamped = Math.Min(t, 1);
+        var val = Math.Min(timeClamped * 4, 1)+ sinVal * (1 - timeClamped) * 0.25;
+        return (float) val;
     }
 }
