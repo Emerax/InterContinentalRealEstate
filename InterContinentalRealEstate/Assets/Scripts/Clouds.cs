42c3a30236cb8e8afb3812521db7653c6f855be2
Erik Mansén <erima668@student.liu.se>
2019-01-26 22:21:26 +0100

    Add clouds to the planet prefab

---
+++ InterContinentalRealEstate/Assets/Scripts/Clouds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clouds : MonoBehaviour {
+
+    public float rotationalSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, rotationalSpeed * Time.deltaTime);
+    }
+}
