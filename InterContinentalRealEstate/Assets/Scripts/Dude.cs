35518724534668840083ff279eef1532744eb3c4
Axel Nordanskog <axeno840@student.liu.se>
2019-01-26 13:39:15 +0100

    Make Dudes roam across planet surface

---
+++ InterContinentalRealEstate/Assets/Scripts/Dude.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dude : MonoBehaviour
+{
+    public Vector3 planetCenter = Vector3.zero;
+
+    private float timeUntilNewDirection = 0.0f;
+    private Quaternion direction = Quaternion.Euler(0, 0, 0);
+    
+    void Start()
+    {
+        timeUntilNewDirection = 0.0f;
+    }
+
+    void Update()
+    {
+        // New movement direction
+        if (timeUntilNewDirection <= 0.0f)
+        {
+            float maxAngle = 2.5f;
+            float maxTime = 5.0f;
+
+            float angleA = (Random.value * 2.0f - 1.0f) * maxAngle;
+            float angleB = (Random.value * 2.0f - 1.0f) * maxAngle;
+            float angleC = (Random.value * 2.0f - 1.0f) * maxAngle;
+
+            direction = Quaternion.Euler(angleA, angleB, angleC);
+            timeUntilNewDirection = Random.value * maxTime;
+        }
+
+        // Move in direction
+        Quaternion deltaRotation = Quaternion.Lerp(Quaternion.Euler(0, 0, 0), direction, Time.deltaTime);
+        transform.position = deltaRotation * (transform.position - planetCenter) + planetCenter;
+        timeUntilNewDirection -= Time.deltaTime;
+
+        // Feet towards planet center
+        transform.LookAt(planetCenter);
+    }
+}
